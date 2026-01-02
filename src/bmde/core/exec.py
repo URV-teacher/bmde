af8a11d12c860853981ca5113882a1e24bdf81ac
AleixMT <aleixaretra@gmail.com>
2026-01-02 06:29:50 +0100

    ADDED: trace debugging in tests

--- src/bmde/core/exec.py
+++ src/bmde/core/exec.py
@@ -31,7 +31,7 @@ def run_cmd(cmd: list[str], opts: ExecOptions) -> int | subprocess.Popen[bytes]:
         pretty = " ".join(cmd)
         args = cmd
 
-    log.debug("exec: %s", pretty)
+    log.trace("exec: %s", pretty)
     if opts.dry_run:
         log.info("[dry-run] %s", pretty)
         return 0
